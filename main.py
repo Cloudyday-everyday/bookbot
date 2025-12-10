ec4be9e2df95df33f21b446a0f6749a41728ad48
Cloudy_Skies <leisure.jtempleton@gmail.com>
2025-12-11 06:49:18 +0800

    Project complete!

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import os
+import sys 
 from stats import count_words
+from stats import count_chars
+from stats import sort_dict
 
-filepath = './books/frankenstein.txt'
 
-def get_text(filepath):
+if len(sys.argv) ==2:
+    filepath = sys.argv[1]
+
+else:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+
+
+
+
+def get_text(filepath = filepath):
     with open(filepath) as f:
-        file_content = f.read()
+        file_content = f.read().lower()
     return file_content
 
-def main(filepath):
-    text = str(get_text(filepath))
+def main():
+    text = str(get_text())
     print (text)
 
-count_words(get_text(filepath))
+
+def program():
+    print (f'''========== BOOKBOT ==========
+            Analyzing book found at {filepath}...
+            --------- WORD COUNT --------
+            {count_words(get_text())}
+            --------- CHARACTER COUNT ---
+            ''')
+    for i,j in (sort_dict(count_chars(get_text()))):
+        print(f'{i}: {j}')
+    print('======== END =======')
+
+program()
--- stats.py
+++ stats.py
@@ -2,4 +2,14 @@ import os
 
 def count_words(text):
     data = text.split()
-    print(f'Found {len(data)} total words')
+    return (f'Found {len(data)} total words')
+
+def count_chars(text):
+    temp_dict = {}
+    for i in text:
+        temp_dict[i] = temp_dict.get(i, 0) +1
+    return temp_dict
+
+def sort_dict(dictionary):
+    for key, value in (sorted(dictionary.items(), key = lambda x: x[1], reverse= True)):
+        yield(key, value)
